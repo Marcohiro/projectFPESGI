cf1bfd26ba4d8e5ca2b1440cefceb907aaa42423
@Marcohiro <petit.marcalain@yahoo.fr>
2020-12-17 14:00:22 +0100

    More refracto done

--- src/main/scala/projetal2020/Main.scala
+++ src/main/scala/projetal2020/Main.scala
@@ -1,7 +1,7 @@
 package projetal2020
 
 import main.scala.projetal2020.Classes.{DataParser, FileParser, ResWriter}
-
+import main.scala.projetal2020.Exceptions.DonneesIncorectesException
 import better.files._
 import play.api.libs.json._
 
@@ -12,29 +12,27 @@ object Main extends App {
     "C:\\Users\\petit\\OneDrive\\Bureau\\SCALA test\\valide.txt"
   )
 
-  val data = fileParser.getData()
+  try {
+    val data = fileParser.getData()
 
-  val dataParser = new DataParser(data)
+    val dataParser = new DataParser(data)
 
-  val init = dataParser.getInit()
+    val resWriter = new ResWriter(
+      dataParser.getInit(),
+      dataParser.getMowersStart(),
+      dataParser.getInstructions(),
+      dataParser.executeInstructionsSet()
+    )
 
-  val start = dataParser.getMowersStart()
+    val outputFile: File = File(
+      "C:\\\\Users\\\\petit\\\\OneDrive\\\\Bureau\\\\SCALA test\\\\output2.json"
+    )
 
-  val instructions = dataParser.getInstructions()
+    outputFile.createIfNotExists().appendLine(Json.stringify(resWriter.write()))
 
-  val res = dataParser.executeInstructionsSet()
-
-  val resWriter = new ResWriter(
-    init,
-    start,
-    instructions,
-    res
-  )
-
-  val outpuFile: File = File(
-    "C:\\\\Users\\\\petit\\\\OneDrive\\\\Bureau\\\\SCALA test\\\\output2.json"
-  )
-
-  outpuFile.createIfNotExists().appendLine(Json.stringify(resWriter.write()))
-
+    ()
+  } catch {
+    case donneesIncorectesException: DonneesIncorectesException =>
+      println(donneesIncorectesException)
+  }
 }
